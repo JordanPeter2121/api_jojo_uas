ed5a1ecf0e05ce84645d98479a1bea4ca9b2f271
bonar petrus jordan sihombing <bonarpetrusjordan@gmail.com>
2024-12-11 22:39:53 +0700

    first commit6

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// import body parser
+    // import body parser
 const bodyParser = require('body-parser')
+const path = require('path')
 
-
-
-// ini untuk cek website
-app.get('/',(req,res) =>{
-     res.send('Halo jojo')
- })
+app.use(express.static(path.join(__dirname, 'public')));
 
 // insert,edit,delete
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // import route posts
 const postsRouter = require('./routes/posts')
 
 // untuk menggunakan route posts di express
-app.use('/api/posts',postsRouter)
+app.use('/api/posts', postsRouter)
 
-    
+//route ke server html
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'design.html');
+    console.log(`Berhasil Update: $ { filePath }`);
+    // Log the file path
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            // untuk menunjukkan error
+            res.status(err.status).end();
+        }
+    });
+});
+
+
 // untuk akses halaman web
-app.listen(port, ()=> {
-    console.log(`aplikasi berjalan di http://localhost:${port}`)
+app.listen(port, () => {
+    console.log(`
+                aplikasi berjalan di http://localhost:${port}`)
 })
--- routes/posts.js
+++ routes/posts.js
@@ -5,21 +5,21 @@ const router = express.Router()
 const koneksi = require('../config/database')
 
 // import express validator
-const {body, validationResult} = require('express-validator')
+const { body, validationResult } = require('express-validator')
 
 // view
-router.get('/',function (req,res){ 
+router.get('/', function(req, res) {
     // isi data(query)
-    koneksi.query('SELECT * FROM posts ORDER BY id desc ',function(err,rows){
-        if(err){
+    koneksi.query('SELECT * FROM posts ORDER BY id desc ', function(err, rows) {
+        if (err) {
             return res.status(500).json({
-                status:false,
-                message:'Server anda sedang error,kocak!',
+                status: false,
+                message: 'Server anda sedang error,kocak!',
             })
-        }else{
+        } else {
             return res.status(200).json({
                 status: true,
-                message:'Menampilkan data posts',
+                message: 'Menampilkan data posts',
                 data: rows
             })
         }
@@ -27,78 +27,78 @@ router.get('/',function (req,res){
 })
 
 // INSERT
-router.post('/store',[
-    // untuk validasi
-    body('nama').notEmpty(),
-    body('jabatan').notEmpty(),
-    body('karakter').notEmpty(),
-    body('ciriciri').notEmpty(),
-    body('Haki').notEmpty(),
-    body('buahiblis').notEmpty(),
-    body('jurusandalan').notEmpty(),
-    body('gender').notEmpty(),
-    body('hargaburonan').notEmpty(),
+router.post('/store', [
+        // untuk validasi
+        body('nama').notEmpty(),
+        body('jabatan').notEmpty(),
+        body('karakter').notEmpty(),
+        body('ciriciri').notEmpty(),
+        body('Haki').notEmpty(),
+        body('buahiblis').notEmpty(),
+        body('jurusandalan').notEmpty(),
+        body('gender').notEmpty(),
+        body('hargaburonan').notEmpty(),
 
-], (req,res)=> {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error:errors.array()
-        })
-    }
-
-    // definisi form data
-    let formData = {
-        nama: req.body.nama,
-        jabatan: req.body.jabatan,
-        karakter: req.body.karakter,
-        ciriciri: req.body.ciriciri,
-        Haki: req.body.Haki,
-        buahiblis: req.body.buahiblis,
-        jurusandalan: req.body.jurusandalan,
-        gender: req.body.gender,
-        hargaburonan: req.body.hargaburonan,
-    }
-
-    // memasukkan Querry / data
-    koneksi.query('INSERT INTO posts SET ?', formData, function(err,rows){
-        // untuk menampilkan error
-        if(err){
-            console.error('error SQL:',err);
-            return res.status(500).json({
-                status: false,
-                message:'Server error',
-            })
-        }else{
-            return res.status(201).json({
-                status: true,
-                message:'Berhasil Masuk',
-                data:rows[0]
+    ], (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                error: errors.array()
             })
         }
+
+        // definisi form data
+        let formData = {
+            nama: req.body.nama,
+            jabatan: req.body.jabatan,
+            karakter: req.body.karakter,
+            ciriciri: req.body.ciriciri,
+            Haki: req.body.Haki,
+            buahiblis: req.body.buahiblis,
+            jurusandalan: req.body.jurusandalan,
+            gender: req.body.gender,
+            hargaburonan: req.body.hargaburonan,
+        }
+
+        // memasukkan Querry / data
+        koneksi.query('INSERT INTO posts SET ?', formData, function(err, rows) {
+            // untuk menampilkan error
+            if (err) {
+                console.error('error SQL:', err);
+                return res.status(500).json({
+                    status: false,
+                    message: 'Server error',
+                })
+            } else {
+                return res.status(201).json({
+                    status: true,
+                    message: 'Berhasil Masuk',
+                    data: rows[0]
+                })
+            }
+        })
     })
-})
-// detail data
-router.get('/:id', function(req,res){
+    // detail data
+router.get('/:id', function(req, res) {
     let id = req.params.id
 
     koneksi.query(`SELECT * FROM posts WHERE ID=${id}`,
-        function(error,rows){
-            if(error){
+        function(error, rows) {
+            if (error) {
                 return res.status(500).json({
-                    status:false,
+                    status: false,
                     message: 'server anda eror',
                 })
             }
             //kalau post id nya belum/tidak ada 
-            if(rows.length <= 0){
+            if (rows.length <= 0) {
                 return res.status(404).json({
-                    status:false,
+                    status: false,
                     message: 'data belum ada'
                 })
             } else {
                 return res.status(200).json({
-                    status:true,
+                    status: true,
                     message: 'menampilan data post berdasarkan id',
                     data: rows[0]
                 })
@@ -108,7 +108,7 @@ router.get('/:id', function(req,res){
 })
 
 //update /edit data
-router.patch('/update/:id',[
+router.patch('/update/:id', [
     body("nama").notEmpty(),
     body("jabatan").notEmpty(),
     body("karakter").notEmpty(),
@@ -119,40 +119,40 @@ router.patch('/update/:id',[
     body("gender").notEmpty(),
     body("hargaburonan").notEmpty(),
 
-],(req,res)=>{
+], (req, res) => {
     const errors = validationResult(req)
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(442).json({
-            errors:errors.array()
+            errors: errors.array()
 
         })
     }
 
     let id = req.params.id //untuk mengambil data id
 
-    let formData={
-        nama: req.body.nama,
-        jabatan: req.body.jabatan,
-        karakter: req.body.karakter,
-        ciriciri: req.body.ciriciri,
-        Haki: req.body.Haki,
-        buahiblis: req.body.buahiblis,
-        jurusandalan: req.body.jurusandalan,
-        gender: req.body.gender,
-        hargaburonan: req.body.hargaburonan,
-}
-//update query
+    let formData = {
+            nama: req.body.nama,
+            jabatan: req.body.jabatan,
+            karakter: req.body.karakter,
+            ciriciri: req.body.ciriciri,
+            Haki: req.body.Haki,
+            buahiblis: req.body.buahiblis,
+            jurusandalan: req.body.jurusandalan,
+            gender: req.body.gender,
+            hargaburonan: req.body.hargaburonan,
+        }
+        //update query
     koneksi.query(`UPDATE posts set ? WHERE id=${id}`, formData,
-        function(err,rows){
-            if(err){
+        function(err, rows) {
+            if (err) {
                 return res.status(500).json({
                     status: false,
-                    message:'Internal Server error'
+                    message: 'Internal Server error'
                 })
-            }else {
+            } else {
                 return res.status(200).json({
                     status: true,
-                    message:'update data berhasil'
+                    message: 'update data berhasil'
                 })
             }
         }
@@ -161,25 +161,25 @@ router.patch('/update/:id',[
 
 //Delete
 router.delete('/delete/(:id)',
-    function(req,res){
+    function(req, res) {
         let id = req.params.id
 
         koneksi.query(`DELETE FROM posts WHERE id = ${id}`,
-          function(error,rows){
-            if(error){
-                return res.status(500).json({
-                    status: false,
-                    message: 'inrternal server error',
-                })
-            }else{
-                return res.status(200).json({
-                    status: true,
-                    message: 'data telah dihapus'
-                })
+            function(error, rows) {
+                if (error) {
+                    return res.status(500).json({
+                        status: false,
+                        message: 'inrternal server error',
+                    })
+                } else {
+                    return res.status(200).json({
+                        status: true,
+                        message: 'data telah dihapus'
+                    })
+                }
             }
-          }  
         )
-    } 
+    }
 )
 
 module.exports = router
